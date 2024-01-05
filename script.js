6927a40bf0f1b32ced95674c5a7d48d66bbe6ebc
saddiecodes <hannahbarrie1999@gmail.com>
2024-01-05 11:49:35 +0000

    password generation code js

--- script.js
+++ script.js
@@ -90,16 +90,48 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+var passwordLength = parseInt(prompt("How long do you want the password to be? It needs to be betweeen 8 and 128 characters."));
 
+if (Number.isNaN(passwordLength)){
+  alert("The length entered must be a number.");
+  return getPasswordOptions; 
+}
+
+if (passwordLength < 8 || passwordLength > 128){
+  alert ("Password length is not between 8 and 128 characters.");
+  return getPasswordOptions;
+}
+
+var includeSpecial = confirm("Do you want to include special characters?");
+var includeUpper = confirm("Do you want to include upper case characters");
+var includeNumbers = confirm("Do you want to include numbers?");
+var includeLower = confirm("Do you want to include lower case characters?"); 
+if (!includeSpecial&& !includeUpper&& !includeNumbers&& !includeLower){
+  alert ("Choose at least one of the character types stated.");
+  return getPasswordOptions;
+}
+
+var userOptions = {
+  length: passwordLength,
+  includeSpecial: includeSpecial, 
+  includeUpper: includeUpper,
+  includeNumbers: includeNumbers,
+  includeLower: includeLower, 
+}
+
+return userOptions
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+var randomIndex = Math.floor(Math.random()* arr.length);
+var randomElement = arr[randomIndex];
+return randomElement; 
 }
 
 // Function to generate password with user input
 function generatePassword() {
+var options = getPasswordOptions();
 
 }
 
